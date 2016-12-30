6bb0db032e3e176e4f5ad74acf35c7d84a7f7a0a
rahulptel <rpatel.1594@gmail.com>
2016-12-30 14:00:15 +0530

    Added style tabs using TabLayout

--- app/src/main/java/com/example/android/miwok/MainActivity.java
+++ app/src/main/java/com/example/android/miwok/MainActivity.java
@@ -17,6 +17,8 @@ package com.example.android.miwok;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.TextView;
@@ -30,58 +32,18 @@ public class MainActivity extends AppCompatActivity {
         // Set the content of the activity to use the activity_main.xml layout file
         setContentView(R.layout.activity_main);
 
-        //Find the text-views of various category
-        TextView numbers = (TextView)findViewById(R.id.numbers);
-        TextView family = (TextView)findViewById(R.id.family);
-        TextView colors = (TextView)findViewById(R.id.colors);
-        TextView phrases = (TextView)findViewById(R.id.phrases);
+        // Find the view pager that will allow the user to swipe between fragments
+        ViewPager viewPager = (ViewPager) findViewById(R.id.viewpager);
 
-        //Set the onClickListen on the numbers view
-        numbers.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                //Create new intent
-                Intent numbersIntent = new Intent(MainActivity.this, NumbersActivity.class);
+        // Create an adapter that knows which fragment should be shown on each page
+        SimpleFragmentPagerAdapter adapter = new SimpleFragmentPagerAdapter(getSupportFragmentManager());
 
-                //Start new activity
-                startActivity(numbersIntent);
-            }
-        });
+        // Set the adapter onto the view pager
+        viewPager.setAdapter(adapter);
 
-        //Set the OnClickListener on the family view
-        family.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                //Create new Intent
-                Intent familyIntent = new Intent(MainActivity.this, FamilyActivity.class);
-                //Start new activity
-                startActivity(familyIntent);
-            }
-        });
-
-
-        //Set the OnClickListener on the colors view
-        colors.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                //Create new Intent
-                Intent colorsIntent = new Intent(MainActivity.this, ColorsActivity.class);
-                //Start new activity
-                startActivity(colorsIntent);
-            }
-        });
-
-        //Set the OnClickListener on the phrases view
-        phrases.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                //Create new Intent
-                Intent phrasesIntent = new Intent(MainActivity.this, PhrasesActivity.class);
-                //Start new activity
-                startActivity(phrasesIntent);
-            }
-        });
-
+        //Give the TabLayout to the ViewPager
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.sliding_tabs);
+        tabLayout.setupWithViewPager(viewPager);
     }
 
 }
